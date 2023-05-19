a7111dd7feb130a5b41c5e7892cfc2cbe8247ce3
YaelZamora <ajedrezkasparov@gmail.com>
2023-05-19 12:59:24 -0600

    Initial Commit

---
+++ widgets/widgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  widgetsApp.swift
+//  widgets
+//
+//  Created by Yael Javier Zamora Moreno on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct widgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
